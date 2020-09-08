ad517ad2db7d70b1bbd3229a54b1bdce5bfb6a76
Akira <31248150+akira2542@users.noreply.github.com>
2020-09-09 00:11:30 +0700

    [ADD] working directories and initial components

---
+++ src/components/sponsor/sponsor.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+// if state are needed change to class component as you se fit - ruangrit 
+
+export default function Sponsor() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
