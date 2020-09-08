264bcb444792638c646b4a6c251c398d87539c08
panus2544 <panus.khjrs@mail.kmutt.ac.th>
2020-09-08 19:29:49 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import logo from './assets/images/logo.svg';
+import Test from './components/Test'
+import '~/App.css'
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+            </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                       <Test name="Dolphin"/>
+            </a>
+                </header>
+            </div>
+        );
+    }
+}
