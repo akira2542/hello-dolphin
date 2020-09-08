52a402e370b848139c009beeb82404e7dc0f643c
Akira <31248150+akira2542@users.noreply.github.com>
2020-09-09 00:14:47 +0700

    [UPDATE] component naming

--- src/components/faqs/faqs.js
+++ src/components/faqs/faqs.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 // if state are needed change to class component as you se fit - ruangrit 
 
-export default function Home() {
+export default function Faqs() {
     return (
         <div>
             
--- src/components/sponsor/sponsorfooter.js
+++ src/components/sponsor/sponsorfooter.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 // if state are needed change to class component as you se fit - ruangrit 
 
-export default function Sponsor() {
+export default function SponsorFooter() {
     return (
         <div>
             
