47a18e0c845fae97b7ed00b4e73b985067114ff1
Meawmfia <boon20002543@hotmail.com>
2020-09-10 14:33:47 +0700

    [UPDATE] Globalstyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import style from '~/App.css' // Use Local CSS Not import '~/App.css' 
 import 'bootstrap/dist/css/bootstrap.min.css' //this is how you import bootstrap
-
+import {createGlobalStyle} from 'styled-components';
 import Navbar from './components/core/navbar/navbar'
 import SideBar from './components/core/navbar/sidebar'
 import Home from './components/home/home'
@@ -9,11 +9,41 @@ import About from './components/about/about'
 import Team from './components/team/team'
 import Faqs from './components/faqs/faqs'
 import Sponsor from './components/sponsor/sponsor'
+import ryeFont1 from './assets/font-family/Rye/Rye-Regular.woff';
+import ryeFont2 from './assets/font-family/Rye/Rye-Regular.woff2';
+import itimFont1 from './assets/font-family/Itim/Itim-Regular.woff';
+import itimFont2 from './assets/font-family/Itim/Itim-Regular.woff2';
+
+
+
+const Globalstyle = createGlobalStyle`
+
+@font-face {
+    font-family: 'Rye-Regular';
+    src: url(${ryeFont1}) format('woff'),url(${ryeFont2}) format('woff2'); 
+    font-style: normal;
+    font-display: swap; 
+
+}
+@font-face {
+    font-family: 'Itim-Regular';
+    src: url(${itimFont1}) format('woff'),url(${itimFont2}) format('woff2'); 
+    font-style: normal;
+    font-display: swap; 
+
+}
+h1{
+    font-family: 'Itim-Regular';
+}
+
+`
 
 export default class App extends React.Component {
     render() {
         return (
             <React.Fragment>
+                <Globalstyle />
+          
                 <Navbar/>
                 <SideBar/>
                 <Home/>
