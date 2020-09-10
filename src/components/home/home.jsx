493b94c31a025181501273b8953684351aac418a
Pathinya <gift_01210@hotmail.com>
2020-09-10 15:53:41 +0700

    [UPDATE] home.jsx structure

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,12 +1,62 @@
 
-import React from 'react'
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import logo from '../../assets/images/home/Logo.png'
+import facebook from '../../assets/images/home/facebook.png'
+import SubHead from '../core/text'
 
-// if state are needed change to class component as you se fit - ruangrit 
+const HomeContainer = styled.div`
+    display: table;
+    position: relative;
+    height: 100vh;
+    width: 100%;
+    z-index: 1;
+    font-family : 'Itim-Regular' ;
+    background-color: #023058;
+`
 
-export default function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+const HomeContent = styled.div`
+    text-align: center;
+    display: table-cell;
+    vertical-align: middle;
+`
+
+const Logo = styled.img`
+    width: 55%;
+`
+
+const Facebook = styled.img`
+    width: 64px;
+    margin-right: 1%;
+`
+
+const Fanpage = styled.a`
+    font-size: 30px;
+    vertical-align: middle;
+    margin-left: 1%;
+    color: white;
+
+    :hover {
+        text-decoration: none;
+        color: white;
+    }
+`
+
+export default class home extends Component {
+    render() {
+        return (
+            <HomeContainer>
+                <HomeContent>
+                    <Logo src={logo} alt="Hello World Dolphin Logo" />
+                    <br />
+                    <Fanpage href="https://www.facebook.com/SITHelloWorld" target="_blank">
+                        <Facebook src={facebook} alt="Facebook Logo" />
+                        SIT HelloWorld
+                    </Fanpage>
+                    
+                    
+                </HomeContent>
+            </HomeContainer>
+        )
+    }
 }
