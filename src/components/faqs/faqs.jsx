51a75879c32a18e8e559bae2eb2e7831d1215458
akira2542 <akiraakamura@gmail.com>
2020-09-10 18:42:52 +0700

    [UPDATE] accordian skeleton

--- src/components/faqs/faqs.jsx
+++ src/components/faqs/faqs.jsx
@@ -1,12 +1,46 @@
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
+import { Header } from '../core/text'
 
-import React from 'react'
+const Container = styled.div`
+width: 100%;
+height: 100vh;
+background-color: #023058;
+`
 
-// if state are needed change to class component as you se fit - ruangrit 
+const AccordianCon = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+`
+const Accordian = styled.div`
+width: 800px;
+border: 1px solid red;
+`
+const AccordianHeading = styled.div`
+width: 100%;
+background-color: #C1D3E8; 
+`
+const Indicator = style 
 
-export default function Faqs() {
-    return (
-        <div>
-            
-        </div>
-    )
+const AccordianContent = styled.div`
+width: 100%;
+background-color: grey;
+`
+
+export default class Faqs extends Component {
+    render() {
+        return (
+            <Container>
+                <Header color="white " textAlign="center" headerText="FAQS" />
+                <AccordianCon>
+                    <Accordian>
+                        <AccordianHeading>Heading</AccordianHeading>
+                        <AccordianContent>Content</AccordianContent>
+                    </Accordian>
+                </AccordianCon>
+            </Container>
+        )
+    }
 }
+
