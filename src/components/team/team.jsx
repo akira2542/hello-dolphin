28a0f739ec258e12f7f79a65db29addbc158cf59
BoatPrakit <boatnakrub_@hotmail.com>
2020-09-16 00:49:40 +0700

    [Update] child can send prop to parent now

--- src/components/team/team.jsx
+++ src/components/team/team.jsx
@@ -11,14 +11,14 @@ import styled from 'styled-components'
 const Button = styled.div`
     color : ${props => props.color};
     cursor : pointer;
-    font-size : 12px;
+    font-size : ${props => props.fontSize};
     border : 2px solid ${props => props.color};
     padding : 10px;
     text-align : center;
     margin : 20px 0 0 0;
     font-family : "Itim-Regular";
     border-radius : 9px;
-    margin : 20px 0;
+    margin : 20px auto;
     width : ${props => props.width};
     :hover {
         color : black;
@@ -36,7 +36,7 @@ const Container = styled.div`
 const SubjectItemContainer = styled.div`
     display : flex;
     width : 100%;
-    align-items : flex-end;
+    align-items : flex-start;
 `
 const SubjectItem = styled.div`
     margin : 10px 10px;
@@ -81,6 +81,7 @@ const Wrapper = styled.div`
 const Content = styled.div`
     font-size : 20px;
     font-family : "Itim-Regular";
+    text-indent : 50px;
 `
 const Column = styled.div`
     display : flex;
@@ -90,31 +91,41 @@ const Column = styled.div`
 
 function Subject(props){
     return (
-        <SubjectItem color={props.color} shadow={props.shadow}>
-            <SubjectLogo  src={props.src} shadow={props.shadow}></SubjectLogo>
-            <Button  color={props.color} shadow={props.shadow}> {props.name} </Button>
+        <SubjectItem color={props.color} shadow={props.shadow} className="active" onClick={props.onClick}>
+            <SubjectLogo  src={props.src} />
+            <Button  color={props.color} fontSize={"12px"} > {props.name} </Button>
         </SubjectItem>
     )
 }
-function TotalSubject(){
+function TotalSubject(props){
     return (
         <SubjectItemContainer>
-            <Subject src={fe_logo}  name="Front-End" color="#B9E6E9" shadow="#31CBED" />
-            <Subject src={web_design_logo}  name="Web Design" color="#E8BEC6" shadow="#DB6CBE" />
-            <Subject src={infra_logo} name="Infrastructure" color="#A8E9A1" shadow="#93EB98" />
-            <Subject src={game_design_logo}  name="Game Design" color="#FFED8C" shadow="#FFEE7C" />
-            <Subject src={game_dev_logo}  name="Game Development" color="#FFAC3A" shadow="#FFA700" />
+            <Subject src={fe_logo}  name="Front-End" color="#B9E6E9" shadow="#31CBED" onClick={() => props.onClick(0)} />
+            <Subject src={web_design_logo}  name="Web Design" color="#E8BEC6" shadow="#DB6CBE" onClick={() => props.onClick(1)} />
+            <Subject src={infra_logo} name="Infrastructure" color="#A8E9A1" shadow="#93EB98" onClick={() => props.onClick(2)}  />
+            <Subject src={game_design_logo}  name="Game Design" color="#FFED8C" shadow="#FFEE7C" onClick={() => props.onClick(3)}  />
+            <Subject src={game_dev_logo}  name="Game Development" color="#FFAC3A" shadow="#FFA700" onClick={() => props.onClick(4)}  />
         </SubjectItemContainer>
     )
 }
 export default class Team extends Component {
-        state = {
-            subject_header : "Front-End",
-            subject_description : "ในสาขานี้จะพาไปเปิดโลกของการเป็นนักพัฒนาเว็บไซต์ ซึ่งจะได้เรียนรู้ตั้งเเต่ HTML & CSS, Javascript เเละการใช้ git จนถึงการเขียน React ซึ่งเป็นสิ่งที่มีความต้องการสูง ในสายงาน IT ในปัจจุบัน"
-        }
+    state = {
+        subject_header : "Front-End",
+        subject_description : [
+            `ในสาขานี้จะพาไปเปิดโลกของการเป็นนักพัฒนาเว็บไซต์ ซึ่งจะได้เรียนรู้ตั้งเเต่ HTML & CSS,
+            Javascript เเละการใช้ git จนถึงการเขียน React ซึ่งเป็นสิ่งที่มีความต้องการสูง ในสายงาน IT ในปัจจุบัน`,
+            `2`
+        ] 
+    }
 
 
-    handleEvent = ()=> {}
+    handleClick = (index)=> {
+        const activeElement = document.getElementsByClassName("active");
+        const pElement = document.getElementsByClassName('content')[0];
+
+        // activeElement[index].style.  
+        pElement.innerHTML = this.state.subject_description[index];
+    }
 
     render() {
         return (
@@ -125,12 +136,16 @@ export default class Team extends Component {
                         <Header>Teams</Header>
                         <section className="subject-zone">
                             <SubHeader>{this.state.subject_header}</SubHeader>
-                            <Content>{this.state.subject_description}</Content>
-                            <Button color="#B9E6E9" shadow="#31CBED" width="100px">Register</Button>
+                            <Content><p className="content">{this.state.subject_description[0]}</p></Content>
+                            <Button 
+                                color="#B9E6E9" 
+                                shadow="#31CBED"
+                                width="200px" 
+                                fontSize="18px" > Register </Button>
                         </section>
                         <section className="other-subject-zone">
                             <OtherSubject>สาขาอื่นๆ</OtherSubject>
-                            <TotalSubject />
+                            <TotalSubject onClick={(i) => this.handleClick(i)} />
                         </section>
                     </Column> 
                     <Column>
