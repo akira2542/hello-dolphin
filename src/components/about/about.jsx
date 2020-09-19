59d431cceade8a06ba1a5606c2e31b400add5df4
Meawmfia <boon20002543@hotmail.com>
2020-09-19 13:50:28 +0700

    [UPDATE] about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,113 @@
 
-import React from 'react'
-
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components';
+import { Header, Content } from '../core/text'
+import cloudLeft from '../../assets/images/about/cloud_left.png'
+import cloudRight from '../../assets/images/about/cloud_right.png';
+import clay from '../../assets/images/about/clay.png';
+import Footer from './aboutfooter'
 // if state are needed change to class component as you se fit - ruangrit 
+const AboutContainer = styled.div`
+background-color:#FF8782;
+padding-bottom: 13.5em;
+position: relative;
 
-export default function AboutFooter() {
-    return (
-        <div>
-            
-        </div>
-    )
+`
+const CloudLeft = styled.img`
+position: absolute;
+right: 55em;
+bottom: 0em;
+/* margin-right: 150%; */
+width: 55%;
+@media (min-width: 1024px){
+    left: 0em;    
+    margin-top: 10em;
+}
+`
+const CloudRight = styled.img`
+position: absolute;
+left:50em; 
+width: 65%;
+bottom: 10em;
+`
+const AboutBorder = styled.div`
+margin-top: 16em;
+margin-bottom: 20em;
+
+padding: 20px;
+border-radius: 30px;
+border: 1px ;
+background-color: rgba(252, 204, 171, 0.52);
+
+@media (min-width: 1024px){
+    width: 50em;
+    height: 30em;
+    margin-left: 4em;
+}
+`
+const ClayTop = styled.img`
+width: 90%;
+margin-top: -4.5em;
+
+`
+
+
+export default class About extends Component {
+    render() {
+        return (
+            <Fragment>
+                <AboutContainer>
+               
+ <ClayTop src={clay} />
+                    <div className="container">
+                        
+                    <div className="row">
+                        
+                        <div className="col-4">
+                        </div>
+                        <div className="col-4">
+                            <Header color="black" headerText="About" />
+                        </div>
+                        <div className="col-4">
+                        </div>
+                    </div>     
+                  {/* <CloudLeft src={cloudLeft} /> */}
+                
+                    <div className="row">
+                        <div className="col-auto">
+                            <AboutBorder>
+                            <Content
+                                
+                                textIndent="24px"
+                                textIndentIpad="50px"
+                                textIndentDesktop="50px"                                
+                                textAlign="่justify"
+                                color="black"
+                                content="Hello World - Dolphin เป็นโครงการที่จัดขึ้นเพื่อแบ่งปันความรู้ในด้านการสร้างเว็บไซต์
+                    และเกม mโดยจะมีการจัดการเรียน การสอนให้กับนักศึกษาที่เข้าร่วมโครงการสำหรับโครงการในปีนี้จะมีสาขาให้นักศึกษาได้เลือกตามความสนใจทั้งหมด 5 สาขา 
+                    คือ Front-End, Web Design, Infrastructure, Game Development และ Game Design"
+                            />
+                               <Content
+                                
+                                textIndent="24px"
+                                textIndentIpad="50px"
+                                textIndentDesktop="50px"                                
+                                textAlign="่justify"
+                                color="black"
+                                content="นอกจากนี้ในช่วงท้ายของโครงการนักศึกษาที่เข้าร่วมโครงการในแต่ละสาขาจะได้มาร่วมกันสร้างเว็บไซต์และเกม ที่เกิดจากความสามารถของนักศึกษาเอง โดยในโครงการ Hello World - Dolphin 
+                    นักศึกษาที่เข้าร่วมโครงการนั้นจะมีบทบาท เป็นโลมาที่พยายามเดินทางไปหาดวงดาวในตำนานที่ถูกเล่าขานต่อ ๆ 
+                    กันมาซึ่งเปรียบเสมือนกับนักศึกษาที่เข้าร่วมโครงการ ที่เดินทางตามหาความฝันและเส้นทางเดินของตนเองนั่นเอง"
+                            />
+                            </AboutBorder>
+                            {/* <CloudRight src={cloudRight}/> */}
+                        </div>
+                    </div>
+                </div>
+                <Footer />
+
+                </AboutContainer>
+
+            </Fragment>
+        )
+    }
 }
--- src/components/about/aboutfooter.jsx
+++ src/components/about/aboutfooter.jsx
@@ -1,12 +1,208 @@
 
-import React from 'react'
+import React, { Component, Fragment } from 'react'
+import styled from 'styled-components'
+import sunrise from '../../assets/images/about/sunrise.png'
+import sunshadow from '../../assets/images/about/sun_shadow.png'
+import boat from '../../assets/images/about/boat.png'
+import waterblue from '../../assets/images/about/water_blue.png'
+import waterdark from '../../assets/images/about/water_darkblue.png'
+import waterpurple from '../../assets/images/about/water_purple.png'
+import waterpink from '../../assets/images/about/water_pink.png'
+
+const Waterblue = styled.img`
+position: absolute;
+width: 100%;
+bottom: 4em;
+right: 0em;
+@media (min-width: 375px){
+    bottom: 0%;
+}
+@media (min-width: 1024px){
+    
+    bottom: 3em;
+}
+`
+const Waterdark = styled.img`
+position: absolute;
+width: 100%;
+@media (min-width: 375px){
+    bottom: -0.5%;
+}
+@media (min-width: 1024px){
+    bottom: -1em;
+}
+
+`
+const Waterpurple = styled.img`
+position: absolute;
+width: 100%;
+bottom: 1em;
+@media (min-width: 375px){
+    bottom: 0;
+}
+@media (min-width: 1024px){
+    
+    bottom: 0;
+}
+
+`
+const Waterpink = styled.img`
+position: absolute;
+width: 100%;
+bottom: 5em;
+@media (min-width: 375px){
+    bottom: 1%;
+}
+@media (min-width: 1024px){
+    bottom: 3em;
+}
+
+`
+const Sunrise = styled.img`
+position: absolute;
+left: 51.5%;
+transform: translateX(-50%);
+width: 20%;
+bottom: 12%;
+
+ @media (min-width: 375px){
+    bottom: 3%;
+}
+@media (min-width: 441px) {
+        bottom: 4%;
+    }
+@media (min-width: 554px){
+    bottom: 5%;
+}
+@media (min-width: 554px){
+    bottom: 6%;
+}
+@media (min-width: 884px){
+    bottom: 7%;
+}
+@media (min-width: 984px){
+    bottom: 8%;
+}
+@media (min-width: 1024px){
+    bottom: 11%;
+}
+
+
+/* @media (min-width: 1024px){
+    left: 33em;
+    bottom: 13em;
+} */
+/* @media (min-width: 1040px){
+    left: 27em;
+    bottom: 10em;
+}
+
+@media (min-width: 1080px){
+    left: 28em;
+    bottom: 11em;
+}
+@media (min-width: 1120px){
+    left: 29em;
+}
+@media (min-width: 1180px){
+    left: 30em;
+}
+@media (min-width: 1220px){
+    left: 32em;
+    
+} */
+`
+const Sunshadow = styled.img`
+position: absolute;
+left: 50%;
+transform: translateX(-50%);
+width: 40%;
+bottom: 6.5%;
+@media (min-width: 375px){
+    bottom: 1.5%;
+}
+@media (min-width: 441px) {
+        bottom: 2%;
+    }
+    @media (min-width: 738px){
+    bottom: 2.5%;
+}
+@media (min-width: 554px){
+    bottom: 3%;
+}
+@media (min-width: 884px){
+    bottom: 3.5%;
+}
+@media (min-width: 984px){
+    bottom: 4%;
+}
+@media (min-width: 1024px){
+    bottom: 6.5%;
+}
+@media (min-width: 1024px){
+    bottom: 7%;
+}
+/* 
+@media (min-width: 1040px){
+    left: 21.5em;
+    bottom: 7.5em;
+}
+@media (min-width: 1080px){
+    bottom: 8em;
+}
+@media (min-width: 1120px){
+    left: 22.5em;
+    bottom: 8.5em;
+    
+
+}
+@media (min-width: 1180px){
+    left: 23.5em;
+    bottom: 9em;
+
+    
+
+}
+@media (min-width: 1280px){
+    left: 26.5em;
+    bottom: 8.2em;
+    height: 12%;    
+} */
+
+
+
+`
+const Boat = styled.img`
+position: absolute;
+bottom: 0em;
+width: 55%;
+@media (min-width: 1024px){
+    left: -3em;
+    bottom: -2.6em;
+    width: 60%;
+}
+@media (min-width: 375px){
+    bottom: -0.5%;
+}
+`
+
 
 // if state are needed change to class component as you se fit - ruangrit 
 
-export default function About() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default class aboutfooter extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <Sunrise src={sunrise} />
+                    <Waterblue src={waterblue} />
+                    <Sunshadow src={sunshadow} />
+                    <Waterpink src={waterpink} />
+                    <Waterpurple src={waterpurple} />
+                    <Boat src={boat} />
+                    <Waterdark src={waterdark} />
+                </div>
+            </Fragment>
+        )
+    }
 }
