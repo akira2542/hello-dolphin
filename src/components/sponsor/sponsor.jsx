96e7199a1987d9e03254fa37360516682bf7b7ed
panus2544 <panus.khjrs@mail.kmutt.ac.th>
2020-10-04 21:12:27 +0700

    [UPDATE] Sponsor

--- src/components/sponsor/sponsor.jsx
+++ src/components/sponsor/sponsor.jsx
@@ -1,22 +1,41 @@
 
-import React from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import { Header } from '../core/text'
 import Footer from './sponsorfooter'
 // if state are needed change to class component as you se fit - ruangrit 
 
 const Container = styled.div`
+padding-top:12vh;
 width: 100%;
-height: 100vh;
+height: auto;
 background-color: #023058;
 /* background: linear-gradient(135deg, #00103a 0%, #023058 100%); */
 `
+const Border = styled.div`
+width : 100%;
+height: 518px;
+border-radius : 28px;
+background-color:rgb(176, 232, 255,0.85);
+`
+
 
 export default function Sponsor() {
     return (
-        <Container>
-            <Header color="white " textAlign="center" headerText="Sponsor" />
-            <Footer/>
-        </Container>
+        <Fragment>
+            <Container>
+                <Header color="white " textAlign="center" headerText="Sponsor" />
+                <div className="row pt-5">
+                    <div className="col">
+                    </div>
+                    <div className="col-10">
+                        <Border />
+                    </div>
+                    <div className="col">
+                    </div>
+                </div>
+                <Footer />
+            </Container>
+        </Fragment >
     )
 }
--- src/components/sponsor/sponsorfooter.jsx
+++ src/components/sponsor/sponsorfooter.jsx
@@ -1,12 +1,34 @@
 
-import React from 'react'
-
-// if state are needed change to class component as you se fit - ruangrit 
-
+import React, { Fragment } from 'react'
+import styled from 'styled-components'
+import kmuttsit from '../../assets/images/sponsor/KMUTTnSIT-01.png'
+const Footer = styled.div`
+  	margin-top: 50px;
+    width: 100%;
+    height: 119px;
+  	background-color : #B0CCE5;
+`
+const Kmuttsit = styled.img`
+    padding-top :10px;
+    padding-bottom : 10px;
+    width : 409px;
+    height : 97px;
+    max-height: 100%;
+`
 export default function SponsorFooter() {
     return (
-        <div>
-            
-        </div>
+        <Footer>
+            <div className="row">
+                <div className="col-4">
+
+                </div>
+                <div className="col-4">
+                    <Kmuttsit src={kmuttsit} alt="kmuttsit" className="mt-2"/>
+                </div>
+                <div className="col-4">
+
+                </div>
+            </div>
+        </Footer>
     )
 }
