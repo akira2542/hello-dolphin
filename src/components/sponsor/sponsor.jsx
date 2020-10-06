73f1a99d1211e772be8ead934259574ac055b0f1
panus2544 <panus.khjrs@mail.kmutt.ac.th>
2020-10-06 21:14:30 +0700

    [UPDATE]Sponsor

--- src/components/sponsor/sponsor.jsx
+++ src/components/sponsor/sponsor.jsx
@@ -2,37 +2,29 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import { Header } from '../core/text'
-import kmuttsit from '../../assets/images/sponsor/KMUTTnSIT-01.png'
 // if state are needed change to class component as you se fit - ruangrit 
 
 const Container = styled.div`
-    padding-top:12vh;
+    padding-top:10vh;
     width: 100%;
     height: 100%;
     background-color: #023058;
+    position: relative;
+    // min-height: 100vh;
+    // min-height: -webkit-fill-available;
+    z-index:1;
 `
 const Border = styled.div`
     width : 100%;
     height: 518px;
+    margin:auto;
     -webkit-border-radius: 27px;
     -moz-border-radius: 27px;
     border-radius: 27px;
     background-color:rgb(176, 232, 255,0.85);
+    z-index:1000;
 `
 
-const Footer = styled.div`
-  	margin-top: 50px;
-    width: 100%;
-    height: 119px;
-    background-color : #B0CCE5;
-    max-width:100%;
-`
-const Kmuttsit = styled.img`
-    padding-top :10px;
-    padding-bottom : 10px;
-    width : 409px;
-    height : 97px;
-`
 
 
 export default function Sponsor() {
@@ -40,28 +32,18 @@ export default function Sponsor() {
         <Fragment>
             <Container>
                 <Header color="white " textAlign="center" headerText="Sponsor" />
-                <div className="row pt-5">
-                    <div className="col">
+                <div className="row pt-5" style={{
+                    marginLeft: "0px",
+                    marginRight: "0px",
+                }}>
+                    <div className="col" style={{zIndex:0}}>
                     </div>
                     <div className="col-10">
                         <Border></Border>
                     </div>
-                    <div className="col">
+                    <div className="col" style={{zIndex:0}}>
                     </div>
                 </div>
-                <Footer>
-                    <div className="row">
-                        <div className="col-2">
-
-                        </div>
-                        <div className="col-8">
-                            <Kmuttsit src={kmuttsit} alt="kmuttsit" className="mt-2 img-fluid mx-auto d-flex justify-content-center" />
-                        </div>
-                        <div className="col-2">
-
-                        </div>
-                    </div>
-                </Footer>
             </Container>
         </Fragment >
     )
--- src/components/sponsor/sponsorfooter.jsx
+++ src/components/sponsor/sponsorfooter.jsx
@@ -1,36 +1,108 @@
 
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
-import kmuttsit from '../../assets/images/sponsor/KMUTTnSIT-01.png'
+import kmuttsit from '../../assets/images/sponsorfooter/KMUTTnSIT-01.png'
+import underoceancoral from '../../assets/images/sponsorfooter/underocean_coral.png'
+import underoceanturtle from '../../assets/images/sponsorfooter/underocean_turtle.png'
+const Container = styled.div`
+    padding-top:350px;
+    width: 100%;
+    height: 100%;
+    background-color: #023058;
+    position: relative;
+    // min-height: 100vh;
+    // min-height: -webkit-fill-available;
+`
 const Footer = styled.div`
   	margin-top: 50px;
     width: 100%;
     height: 119px;
-  	background-color : #B0CCE5;
+    background-color : #B0CCE5;
+    max-width:100%;
 `
+const Underocean = styled.img`
+    position: absolute;
+    right: 100%;
+    bottom: 23%;
+    left: -0.24%;
+    width: 100%;    
+    z-index:1;
+`
+
+const Underturtle = styled.img`
+    position: absolute;
+    width: 30%;
+    z-index:20;
+
+    top:220px; 
+    left:130px; 
+    max-width:575px; 
+    
+    //  @media (min-width: 375px){
+    //      top:220px; 
+    // left:130px; 
+    //  }
+    //  @media (min-width: 411px){
+    //     top:220px; 
+    // left:130px; 
+    //  }
+    //  @media (min-width: 768px){
+    //      top:220px; 
+    // left:130px; 
+    //  }
+    //  @media (min-width: 1024px){
+    //      top:220px; 
+    // left:130px; 
+    //  }
+    //  @media (min-width: 1140px){
+    //    top:220px; 
+    // left:130px; 
+
+    //  }
+    //  @media (min-width: 1280px){
+    //      top:220px; 
+    // left:130px; 
+
+    //  }
+    //  @media (min-width: 1920px){
+    //      top:220px; 
+    //     left:130px; 
+
+    //  }
+`
+
 const Kmuttsit = styled.img`
     padding-top :10px;
     padding-bottom : 10px;
     width : 409px;
     height : 97px;
-    max-height: 100%;
+    
 `
+
+
 export default function SponsorFooter() {
     return (
         <Fragment>
-            {/* <div style={styless}>
-                <div className="row">
-                    <div className="col-4">
+            <Container>
+                <Underocean src={underoceancoral} />
+                <Underturtle src={underoceanturtle} />
+                <Footer>
+                    <div className="row" style={{
+                        marginLeft: "0px",
+                        marginRight: "0px",
+                    }}>
+                        <div className="col-2" style={{ zIndex: 1 }}>
 
-                    </div>
-                    <div className="col-4">
-                        <Kmuttsit src={kmuttsit} alt="kmuttsit" className="mt-2" />
-                    </div>
-                    <div className="col-4">
+                        </div>
+                        <div className="col-8">
+                            <Kmuttsit src={kmuttsit} alt="kmuttsit" className="mt-2 img-fluid mx-auto d-flex justify-content-center" />
+                        </div>
+                        <div className="col-2" style={{ zIndex: 1 }}>
 
+                        </div>
                     </div>
-                </div>
-            </div> */}
+                </Footer>
+            </Container>
         </Fragment>
     )
 }
